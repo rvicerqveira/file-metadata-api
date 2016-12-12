e25958ef6451539f99a1562656a2715b6580498b
Rui Cerqueira <rvicerqveira@gmail.com>
2016-12-12 16:51:15 +0000

    completed challenge

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express')
 var app = express()
+var multer  = require('multer')
+var path = require('path')
+var fs = require('fs')
+require('dotenv').config({silent: true})
 var port = process.env.PORT || 8080
 
+app.use(express.static(path.join(__dirname, './public')))
+app.set('views', path.join(__dirname, "./templates"))
+app.set('view engine', 'jade')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index', { title: 'File Metadata Microservice' })
+})
+
+app.post('/', multer({ dest: 'uploads/' }).single('file'), function(req, res) {
+  var fileSize
+  var fileDetails={}
+  if (req.file) { 
+    fileDetails={bytes: req.file.size} 
+    fs.unlinkSync(path.join(__dirname, "./uploads/" + req.file.filename))
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(fileDetails))
 })
 
 app.listen(port, function () {
